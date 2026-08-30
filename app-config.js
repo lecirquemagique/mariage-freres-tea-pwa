17527001470b1332d9b25f6ee340d7fabb9a7010
lecirquemagique <lecirquemagique@gmail.com>
2026-08-30 16:32:28 +0900

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,12 +1,11 @@
 window.MF_APP_CONFIG = {
   // GASを「ウェブアプリ」としてデプロイした後、/exec のURLをここへ貼り付けます。
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbws6DFk7pT3kppzRSC8J7dSfWCI4zgvbse6LsoGXqPS-BDbB67DBF8_rKhFu7c7Y3fs7A/exec',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbyQAoqDwFolqrp_-wQvxPbGGOj8YyjbXT5bp4dgyns_-DIBeyuF-3DblJFmQgpKhUvzcQ/exec',
 
   // 任意。My Driveに置いた装飾画像の共有URLを貼れます。
   // 画像ファイル単体を「リンクを知っている全員：閲覧者」にしてください。
   HERO_IMAGE_URL: 'https://drive.google.com/file/d/1-wuwh5VVFYCUtvlb5dwzOGXfrgVk6Ix2/view?usp=drive_link',
   LOGO_IMAGE_URL: '',
-
   APP_TITLE: 'LE MAÎTRE EN THÉ',
   APP_SUBTITLE: 'MARIAGE FRÈRES · PARIS 1854'
 };
